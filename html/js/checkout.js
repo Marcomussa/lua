37ac5a34dea8cbd2502c6d3999be151dc088cb7d
Marco <marcomussa567@gmail.com>
2024-05-30 17:23:33 -0300

    k

--- html/js/checkout.js
+++ html/js/checkout.js
@@ -3,6 +3,119 @@ document.addEventListener('DOMContentLoaded', function() {
     updateTotal();
 });
 
+let form = document.querySelector("#order-form")
+let customer = document.querySelector("#name-order")
+let phone = document.querySelector("#phone-order")
+let email = document.querySelector("#email-order")
+let state = document.querySelector("#state-order")
+let street = document.querySelector("#address-order")
+let floor = document.querySelector("#floor-order")
+let city = document.querySelector("#city-order")
+let zip = document.querySelector("#zip-order")
+let addressDetails = document.querySelector("#details-order")
+
+function tests(){
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
+        getOrderData()
+    })
+}
+tests()
+
+async function quotation() {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || []
+    const itemsArray = generateItemsArray(cartItems)
+    let quotation = {
+        "address_from": {
+            // Detalles de Lua Cup
+        },
+        "address_to": {
+            "name": customer.value,
+            "company": addressDetails.value,
+            "street1": street.value,
+            "city": city.value,
+            "state": state.value,
+            "zip": zip.value,
+            "phone": phone.value,
+            "email": email.value,
+            "country": "MX"
+        },
+        "parcels":[
+            {
+                "length":30,
+                "height":20,
+                "width":10,
+                "distance_unit": "cm",
+                "weight": 1, 
+                "mass_unit":"kg",
+                "reference": null
+            }
+        ],
+        "items": itemsArray,
+    }
+    const url = 'https://apiqa.myeship.co/rest/quotation'; 
+    const apiKey = ''; 
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': apiKey
+            },
+            body: JSON.stringify(order)
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        const data = await response.json();
+        console.log(data); // Maneja la respuesta de la API aquí
+    } catch (error) {
+        console.error('Hubo un problema con la petición Fetch:', error);
+    }
+}
+
+function generateItemsArray(cartItems) {
+    return cartItems.map(item => {
+        return {
+            SKU: null,
+            description: item.name,
+            quantity: item.quantity,
+            price: item.price,
+            weight: "",
+            currency: "MEX",
+        };
+    });
+}
+
+function getOrderData() {
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    const itemsArray = generateItemsArray(cartItems);
+
+    const orderData = {
+        address_from: {
+            // Detalles de Lua Cup
+        },
+        address_to: {
+            name: customer.value,
+            company: addressDetails.value,
+            street1: street.value,
+            city: city.value,
+            state: state.value,
+            zip: zip.value,
+            phone: phone.value,
+            email: email.value,
+            country: "MX"
+        },
+        items: itemsArray,
+    }
+
+    console.log(JSON.stringify(orderData))
+    return orderData
+}
+
 function renderCartItems() {
     const cartItemsContainer = document.getElementById('stored-items');
     cartItemsContainer.innerHTML = '';
