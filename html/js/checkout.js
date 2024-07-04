2be44a71ee058a4e8d9168b7f3887643dad4c896
Marcomussa <marcomussa567@gmail.com>
2024-07-04 14:29:05 -0300

    k

--- html/js/checkout.js
+++ html/js/checkout.js
@@ -123,6 +123,9 @@ async function createOrder(shipmentPrice, shipmentProvider, shipmentDays){
 
     const response = await fetch('https://luacup.onrender.com/create-order', {
         method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify(order)
     })
     const data = await response.json()
